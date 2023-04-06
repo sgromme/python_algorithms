bc184fe9ef27508323883f7a1e5dd418ac819010
sgromme <wills1377@gmail.com>
2023-04-06 09:12:42 -0400

    'updated'

--- dictionary.py
+++ dictionary.py
@@ -1,1 +1,31 @@
 # examples of dictionarys
+
+import collections
+
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s) == sorted(t)
+        #return Counter(s) == Counter(t)
+        
+        
+    
+    
+    
+        
+        
+
+s1 = {'a':1, 'b':5}
+
+s2 = {'b':5, 'a':1}
+
+print(collections.Counter(s1))
+
+print(collections.Counter(s1)==collections.Counter(s2))
+
+
