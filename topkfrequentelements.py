1cf1364679a9cee6c13366de70b0c9dfc15d4547
sgromme <wills1377@gmail.com>
2023-05-01 14:25:36 -0400

    updated

--- topkfrequentelements.py
+++ topkfrequentelements.py
@@ -20,24 +20,24 @@ class Solution(object):
         for i in nums:
             dictNums[i] += 1
             
-        print(dictNums)
-        
+                
         # create a list of lists and the list index will be the count and the element
         # will be a list of numbers with that count , we are not using index 0
         count = [[] for i in range(len(nums) + 1 )]
-        #count = [[]] * (len(nums) + 1)
+        
         
         # populate the count list
         for i, v in dictNums.items():
             count[v].append(i)
         
-        print(count)
+       
         # return list of numbers
         ret = []
         
         # loop count from the end
         # the zero really be 1 because count[0] is not used.
         # but it shouldn't matter because we aren't using it.
+        # example range(len(count)-1, 1, -1)
         for v in range(len(count) -1, 0,-1):
             for n in count[v]:
                 ret.append(n)
