6f1a28d93e7bae2f1ba81ed5ec9befb16c47b066
sgromme <wills1377@gmail.com>
2023-05-24 14:17:05 -0400

    commit.

---
+++ encodeanddecodestrings.py
@@ -0,0 +1,76 @@
+class Solution():
+    
+    
+    
+    def encode(self, strs):
+        """
+    :type str: List(string)
+    :return type: string
+    
+    """ 
+        temp = ""
+        for val in strs:
+            temp += str(len(val)) + "#" + val
+        return temp
+    
+    
+    def decode(self, str):
+        """
+        :type str: string
+        :return type List(str)
+        """ 
+        ret = []
+        i = 0
+        j = 0
+        while i < len(str):
+            if str[i] == "#":
+                print(str[j:i])
+                length = int(str[j:i])
+                ret.append(str[i + 1: i + 1 + length])
+                i = i + 1 + length
+                j = i
+            i += 1
+        return ret
+    
+    
+
+class Solution2:
+    """
+    @param: strs: a list of strings
+    @return: encodes a list of strings to a single string.
+    """
+    def encode(self, strs):
+        # write your code here
+        pass
+
+    """
+    @param: str: A string
+    @return: dcodes a single string to a list of strings
+    """
+    def decode(self, str):
+        # write your code here
+        pass
+
+#Input: ["we", "say", ":", "yes"]
+#Output: ["we", "say", ":", "yes"]
+
+#Input: ["lint","code","love","you"]
+#Output: ["lint","code","love","you"]
+
+
+       
+s = ["test","bubba", "#4test"]
+
+classSolution = Solution()
+print(classSolution.encode(s))
+
+r = classSolution.encode(s)
+
+print(classSolution.decode(r))
+
+
+
+
+
+
+
